b4d78d1a2e6a70fcfab6eea8e1d1a258cc2b148e
RMMerengue <1017703403@qq.com>
2022-03-10 15:05:48 +0800

    依赖注入

--- Spring_IOC/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ Spring_IOC/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -1,8 +1,41 @@
 package com.itheima.dao.impl;
 
 import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class UserDaoImpl implements UserDao {
+
+    private List<String> stringList;
+    private Map<String, User> userMap;
+    private Properties properties;
+
+    private String userName;
+    private int age;
+
+    public void setStringList(List<String> stringList) {
+        this.stringList = stringList;
+    }
+
+    public void setUserMap(Map<String, User> userMap) {
+        this.userMap = userMap;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public UserDaoImpl() {
         System.out.println("UserDaoImpl 创建...");
     }
@@ -16,6 +49,10 @@ public class UserDaoImpl implements UserDao {
     }
 
     public void save() {
+        System.out.println(userName + "===" + age);
+        System.out.println(stringList);
+        System.out.println(userMap);
+        System.out.println(properties);
         System.out.println("save running...");
     }
 }
