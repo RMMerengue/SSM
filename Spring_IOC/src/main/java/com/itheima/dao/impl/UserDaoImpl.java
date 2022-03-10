809f2905fd080eed480a4fa01e13687ad3995537
RMMerengue <1017703403@qq.com>
2022-03-10 13:24:09 +0800

    Bean的生命周期&&三种实例化方式：无参构造、工厂静态、工厂实例

--- Spring_IOC/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
+++ Spring_IOC/src/main/java/com/itheima/dao/impl/UserDaoImpl.java
@@ -3,6 +3,18 @@ package com.itheima.dao.impl;
 import com.itheima.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl 创建...");
+    }
+
+    public void init(){
+        System.out.println("init...");
+    }
+
+    public void destroy(){
+        System.out.println("destroy...");
+    }
+
     public void save() {
         System.out.println("save running...");
     }
---
+++ Spring_IOC/src/main/java/com/itheima/factory/DynamicFactory.java
@@ -0,0 +1,13 @@
+package com.itheima.factory;
+
+import com.itheima.dao.UserDao;
+import com.itheima.dao.impl.UserDaoImpl;
+
+public class DynamicFactory {
+
+    public UserDao getUserDao(){
+        return new UserDaoImpl();
+    }
+
+
+}
