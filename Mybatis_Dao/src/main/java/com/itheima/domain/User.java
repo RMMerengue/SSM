32dbb727c617a98de33cac41bf4348e9bfcb7b5d
RMMerengue <1017703403@qq.com>
2022-07-01 20:50:13 +0800

    Mybatis的Dao层实现

---
+++ Mybatis_Dao/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,40 @@
+package com.itheima.domain;
+
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return pwd;
+    }
+
+    public void setPassword(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
