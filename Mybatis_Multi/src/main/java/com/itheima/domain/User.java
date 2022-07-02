de688872ea9d4ef549162b9880c405e28a005123
RMMerengue <1017703403@qq.com>
2022-07-02 22:27:24 +0800

    Mybatis多表操作

---
+++ Mybatis_Multi/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,81 @@
+package com.itheima.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+    private Date birthday;
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    //描述的是当前用户存在那些订单
+    private List<Order> orderList;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return pwd;
+    }
+
+    public void setPassword(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", birthday=" + birthday +
+                ", orderList=" + orderList +
+                '}';
+    }
+}
